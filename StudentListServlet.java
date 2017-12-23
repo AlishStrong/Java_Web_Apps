80435dffc04bb9387bbb0d6b1540696afdbd1339
Alisher Aliev <34649384+AlishStrong@users.noreply.github.com>
2017-12-23 17:47:27 +0200

    Create StudentListServlet.java

---
+++ StudentListServlet.java
@@ -0,0 +1,34 @@
+//Servlet that receives a request from .jsp page and send the right response
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentListServlet
+ */
+@WebServlet("/StudentListServlet")
+public class StudentListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			StudentDAO studentDAO = new StudentDAO();
+			List<Student> studentList = studentDAO.getAllStudents();
+			Collections.sort(studentList);
+			request.setAttribute("studentList", studentList);
+			request.getRequestDispatcher("StudentList.jsp").forward(request, response);
+		} catch (Exception ex) {
+			System.out.println("The database is temporarily unavailable. Please try again later. \n");
+			System.out.println("=== System error message (for the developer's eyes only) === \n" + ex.getMessage());
+		}
+
+	}
+
+}
